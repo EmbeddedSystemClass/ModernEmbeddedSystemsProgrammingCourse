699918e39ce386134ef5915741c8eaaa313f0426
Andrzej Telszewski <atelszewski@gmail.com>
2019-10-06 20:26:04 +0200

    Conclude lesson 7.

--- lesson07/main.c
+++ lesson07/main.c
@@ -3,6 +3,24 @@
 #define PIN8 (1U << 8)
 #define PIN9 (1U << 9)
 
+#define STM32_GPIOC8 (unsigned int volatile *)(0x42000000U + 0x1100CU * 32U + 8U * 4U)
+#define STM32_GPIOC9 (unsigned int volatile *)(0x42000000U + 0x1100CU * 32U + 9U * 4U)
+
+void gpio_high(unsigned int volatile * gpio)
+{
+    *gpio = 1U;
+}
+
+void gpio_low(unsigned int volatile * gpio)
+{
+    *gpio = 0U;
+}
+
+unsigned int volatile * const output[2] = {
+    (unsigned int volatile *)(0x42000000U + 0x1100CU * 32U + 8U * 4U),
+    (unsigned int volatile *)(0x42000000U + 0x1100CU * 32U + 9U * 4U)
+};
+
 /******************************************************************************/
 int main(void)
 /******************************************************************************/
@@ -15,16 +33,18 @@ int main(void)
 
     while (1)
     {
-        GPIOC->BSRR = PIN8;
-        GPIOC->BSR  = PIN9;
+        gpio_high(STM32_GPIOC8);
+        *(output[1]) = 0;
+
         counter = 0;
         while (counter < 240000)
         {
             ++counter;
         }
 
-        GPIOC->BSR  = PIN8;
-        GPIOC->BSRR = PIN9;
+        gpio_low(STM32_GPIOC8);
+        *(output[1]) = 1;
+
         counter = 0;
         while (counter < 240000)
         {
@@ -32,5 +52,5 @@ int main(void)
         }
     }
 
-    return 0;
+    /* return 0; */
 }

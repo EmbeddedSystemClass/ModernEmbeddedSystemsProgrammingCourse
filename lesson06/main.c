f4be86d3236f017484c0234e0c39036ee3eea379
Andrzej Telszewski <atelszewski@gmail.com>
2019-10-06 20:23:54 +0200

    Conclude lesson 6.

--- lesson06/main.c
+++ lesson06/main.c
@@ -1,5 +1,8 @@
 #include "stm32f10x.h"
 
+#define PIN8 (1U << 8)
+#define PIN9 (1U << 9)
+
 /******************************************************************************/
 int main(void)
 /******************************************************************************/
@@ -7,18 +10,21 @@ int main(void)
     int volatile counter;
 
     RCC->APB2ENR |= 0x01U << 4; /* Enable GPIOF clock.  */
-    GPIOC->CRH    = 0x01U;      /* GPIOF8 as PP output. */
+    GPIOC->CRH    = (0x01U << 0) | /* GPIOC8 as PP output. */
+                    (0x01U << 4);  /* GPIOC9 as PP output. */
 
     while (1)
     {
-        GPIOC->BSRR = 1U << 8; /* GPIOC8 high. */
+        GPIOC->BSRR = PIN8;
+        GPIOC->BSR  = PIN9;
         counter = 0;
         while (counter < 240000)
         {
             ++counter;
         }
 
-        GPIOC->BRR  = 1U << 8; /* GPIOC8 low. */
+        GPIOC->BSR  = PIN8;
+        GPIOC->BSRR = PIN9;
         counter = 0;
         while (counter < 240000)
         {

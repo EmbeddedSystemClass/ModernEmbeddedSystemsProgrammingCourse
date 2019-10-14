ab9a6ca2176d85494db21c670937a7cf809a0262
Andrzej Telszewski <atelszewski@gmail.com>
2019-10-14 20:26:07 +0200

    Create copy of lesson 21.

---
+++ lesson22/main.c
@@ -0,0 +1,75 @@
+#include <stdint.h>
+#include "bsp.h"
+
+#if 0
+/* Background code: sequential with blocking version.                         */
+/******************************************************************************/
+int main(void)
+/******************************************************************************/
+{
+    BSP_init();
+
+    while (1)
+    {
+        BSP_ledGreenOn();
+        BSP_delay(BSP_TICKS_PER_SEC / 4U);
+        BSP_ledGreenOff();
+        BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
+    }
+
+    //return 0;
+}
+#endif
+
+/* Background code: non-blocking version.                                     */
+/******************************************************************************/
+int main(void)
+/******************************************************************************/
+{
+    __disable_irq();
+
+    BSP_init();
+
+    while (1)
+    {
+        /* Blinky polling state machine. */
+        static enum {
+            INITIAL,
+            OFF_STATE,
+            ON_STATE
+        } state = INITIAL;
+
+        static uint32_t start;
+
+        switch (state)
+        {
+        case INITIAL:
+            start = BSP_tickCtr();
+            state = OFF_STATE; /* Initial transition. */
+            break;
+
+        case OFF_STATE:
+            if ((BSP_tickCtr() - start) > BSP_TICKS_PER_SEC * 3U / 4U)
+            {
+                BSP_ledGreenOn();
+                start = BSP_tickCtr();
+                state = ON_STATE; /* State transition. */
+            }
+            break;
+
+        case ON_STATE:
+            if ((BSP_tickCtr() - start) > BSP_TICKS_PER_SEC / 4U)
+            {
+                BSP_ledGreenOff();
+                start = BSP_tickCtr();
+                state = OFF_STATE; /* State transition. */
+            }
+            break;
+
+        default:
+            //error();
+            break;
+        }
+    }
+    //return 0;
+}

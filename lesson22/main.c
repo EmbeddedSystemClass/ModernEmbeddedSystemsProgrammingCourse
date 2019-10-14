ba5e7f73881c48717cf424de917c058aca6aedc3
Andrzej Telszewski <atelszewski@gmail.com>
2019-10-14 20:41:51 +0200

    Conclude lesson 22.

--- lesson22/main.c
+++ lesson22/main.c
@@ -1,14 +1,77 @@
 #include <stdint.h>
 #include "bsp.h"
 
-#if 0
-/* Background code: sequential with blocking version.                         */
+uint32_t stack_blinky1[40];
+uint32_t * sp_blinky1 = &stack_blinky1[40];
+uint32_t stack_blinky2[40];
+uint32_t * sp_blinky2 = &stack_blinky2[40];
+
+int main_blinky1(void);
+int main_blinky2(void);
+
 /******************************************************************************/
 int main(void)
 /******************************************************************************/
 {
+    uint32_t volatile run = 0U;
+    __disable_irq();
+
+    /* Fabricate Cortex-M stack frame for blinky1. */
+    *(--sp_blinky1) = (1U << 24);  /* xPSR. */
+    *(--sp_blinky1) =
+        (uint32_t) &main_blinky1;  /* PC.   */
+    *(--sp_blinky1) = 0x0000000EU; /* LR.   */
+    *(--sp_blinky1) = 0x0000000CU; /* R12.  */
+    *(--sp_blinky1) = 0x00000003U; /* R3.   */
+    *(--sp_blinky1) = 0x00000002U; /* R2.   */
+    *(--sp_blinky1) = 0x00000001U; /* R1.   */
+    *(--sp_blinky1) = 0x00000000U; /* R0.   */
+    /* Additionally, fake registers R4 - R11. */
+    *(--sp_blinky1) = 0x0000000BU; /* R11.  */
+    *(--sp_blinky1) = 0x0000000AU; /* R10.  */
+    *(--sp_blinky1) = 0x00000009U; /* R9.   */
+    *(--sp_blinky1) = 0x00000008U; /* R8.   */
+    *(--sp_blinky1) = 0x00000007U; /* R7.   */
+    *(--sp_blinky1) = 0x00000006U; /* R6.   */
+    *(--sp_blinky1) = 0x00000005U; /* R5.   */
+    *(--sp_blinky1) = 0x00000004U; /* R4.   */
+
+
+    /* Fabricate Cortex-M stack frame for blinky1. */
+    *(--sp_blinky2) = (1U << 24);  /* xPSR. */
+    *(--sp_blinky2) =
+        (uint32_t) &main_blinky2;  /* PC.   */
+    *(--sp_blinky2) = 0x0000000EU; /* LR.   */
+    *(--sp_blinky2) = 0x0000000CU; /* R12.  */
+    *(--sp_blinky2) = 0x00000003U; /* R3.   */
+    *(--sp_blinky2) = 0x00000002U; /* R2.   */
+    *(--sp_blinky2) = 0x00000001U; /* R1.   */
+    *(--sp_blinky2) = 0x00000000U; /* R0.   */
+    /* Additionally, fake registers R4 - R11. */
+    *(--sp_blinky2) = 0x0000000BU; /* R11.  */
+    *(--sp_blinky2) = 0x0000000AU; /* R10.  */
+    *(--sp_blinky2) = 0x00000009U; /* R9.   */
+    *(--sp_blinky2) = 0x00000008U; /* R8.   */
+    *(--sp_blinky2) = 0x00000007U; /* R7.   */
+    *(--sp_blinky2) = 0x00000006U; /* R6.   */
+    *(--sp_blinky2) = 0x00000005U; /* R5.   */
+    *(--sp_blinky2) = 0x00000004U; /* R4.   */
+
     BSP_init();
 
+    while (1)
+    {
+        /* Loop forever. */
+        ++run;
+    }
+
+    //return 0;
+}
+
+/******************************************************************************/
+int main_blinky1(void)
+/******************************************************************************/
+{
     while (1)
     {
         BSP_ledGreenOn();
@@ -16,60 +79,17 @@ int main(void)
         BSP_ledGreenOff();
         BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
     }
-
-    //return 0;
 }
-#endif
 
-/* Background code: non-blocking version.                                     */
 /******************************************************************************/
-int main(void)
+int main_blinky2(void)
 /******************************************************************************/
 {
-    __disable_irq();
-
-    BSP_init();
-
     while (1)
     {
-        /* Blinky polling state machine. */
-        static enum {
-            INITIAL,
-            OFF_STATE,
-            ON_STATE
-        } state = INITIAL;
-
-        static uint32_t start;
-
-        switch (state)
-        {
-        case INITIAL:
-            start = BSP_tickCtr();
-            state = OFF_STATE; /* Initial transition. */
-            break;
-
-        case OFF_STATE:
-            if ((BSP_tickCtr() - start) > BSP_TICKS_PER_SEC * 3U / 4U)
-            {
-                BSP_ledGreenOn();
-                start = BSP_tickCtr();
-                state = ON_STATE; /* State transition. */
-            }
-            break;
-
-        case ON_STATE:
-            if ((BSP_tickCtr() - start) > BSP_TICKS_PER_SEC / 4U)
-            {
-                BSP_ledGreenOff();
-                start = BSP_tickCtr();
-                state = OFF_STATE; /* State transition. */
-            }
-            break;
-
-        default:
-            //error();
-            break;
-        }
+        BSP_ledBlueOn();
+        BSP_delay(BSP_TICKS_PER_SEC / 4U);
+        BSP_ledBlueOff();
+        BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
     }
-    //return 0;
 }
